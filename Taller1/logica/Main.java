7e0b39eb34ee78eed97237f21e7f9966bd14e630
EmiToro <emiliano.toro@alumnos.ucn.cl>
2026-03-21 18:34:11 -0300

    Avance de los menu y arreglos
    Avance en los menu y arregle la forma en que se guardan los usuarios y las contrasenias

--- Taller1/logica/Main.java
+++ Taller1/logica/Main.java
@@ -5,39 +5,88 @@ import java.io.FileNotFoundException;
 
 
 public class Main {
-    private String[] Usuarios = [];
-    private String[] contraseña = [];
+    private static String[] Usuarios = new String[2];
+    private static String[] Contraseña = new String[2];
 
-    public static void main(String[] args)Throws Exception {
-        
+    public static void main(String[] args) Throws Exception {
+        Scanner seleccion = new Scanner(System.in);
+        guardar_Usuariio_contra();
+        boolean salir = false;
+        while(!salir){
+        System.out.println("1) Menu de Usuarios");
+        System.out.println("2) Menu de Analisis");
+        System.out.println("3) Salir");
+        int opcion = seleccion.nextInt();
+        seleccion.nextLine();
+
+        if(opcion==1){
+            menu_Usuarios();    
+        }
+        else if(opcion==2){
+            menu_Analisis();
+        }
+        else if(opcion==3){
+            salir=true;
+        }
+
+        }
+
+      seleccion.close();  
     }
 
-    private void guardar_Usuariio_contra()Throws Exception {
+    private static void guardar_Usuariio_contra()Throws Exception {
         Scanner sc = new Scanner(new File("Usuarios"));
+        cont=0;
         while (sc.hasNextLine()) {
             String linea = sc.nextLine();
-            String[] partes = linea.split(",");
-            if (partes.length == 2) {
-                String nombre = partes[0].trim();
-                String contraseña = partes[1].trim();
-                // Aquí puedes almacenar el nombre y la contraseña en tus arreglos o listas
+            String[] partes = linea.split(";");
+            String usuario = partes[0];
+            String contraseña = partes[1];
+
+            if cont=0{
+            Usuarios[0] = usuario;
+            Contraseña[0] = contraseña;
+            }else{
+                Usuarios[cont] = usuario;
+                Contraseña[cont] = contraseña;
             }
+
+            cont+=1;
+            } sc.close();
         }
     }
-    public void static abrirArchivo2(String nombre) throws fileNotFoundException {
 
-        File archivo = new File(nombre)\
-        Scanner teclado = new Scanner(archivo);
+    private static void menu_Usuarios();{
+
+    }
+
+    private static void menu_Analisis() {
+
+    }   
+
+
+    private static void abrirArchivo2() throws FileNotFoundException {
+
+        Scanner teclado = new Scanner(new File("Registros.txt"));
+        int [] CantidadHoras = new int[3];
+
         while (teclado.hasNextLine()) {
             String linea = teclado.nextLine();
-            String[] partes = linea.split(",");
-            if (partes.length == 2) {
-                String nombre = partes[0];
-                String contraseña = partes[1].trim();
-                // Aquí puedes almacenar el nombre y la contraseña en tus arreglos o listas
+            String[] partes = linea.split(";");
+            if (partes.length == 4) {
+                String nombreUsuario = partes[0];
+                String fechas= partes[1];
+                int CantidadHoras= Integer.parseInt(partes[2]);
+                String actividad= partes[3];
             }
         }
-        sc.close();
+        teclado.close();
+    }  
+
+    private static class fileNotFoundException extends Exception {
+
+        public fileNotFoundException() {
+        }
     }
 
 
