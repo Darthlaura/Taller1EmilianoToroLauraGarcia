d5f45ecbd8f9607c181afb481129d3508a9c206b
EmiToro <emiliano.toro@alumnos.ucn.cl>
2026-03-21 17:54:00 -0300

    apertura de archivos
    Se empezo guardando los datos de los archivos

--- Taller1/logica/Main.java
+++ Taller1/logica/Main.java
@@ -1,5 +1,45 @@
 package logica;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 
 public class Main {
+    private String[] Usuarios = [];
+    private String[] contraseña = [];
 
-}
+    public static void main(String[] args)Throws Exception {
+        
+    }
+
+    private void guardar_Usuariio_contra()Throws Exception {
+        Scanner sc = new Scanner(new File("Usuarios"));
+        while (sc.hasNextLine()) {
+            String linea = sc.nextLine();
+            String[] partes = linea.split(",");
+            if (partes.length == 2) {
+                String nombre = partes[0].trim();
+                String contraseña = partes[1].trim();
+                // Aquí puedes almacenar el nombre y la contraseña en tus arreglos o listas
+            }
+        }
+    }
+    public void static abrirArchivo2(String nombre) throws fileNotFoundException {
+
+        File archivo = new File(nombre)\
+        Scanner teclado = new Scanner(archivo);
+        while (teclado.hasNextLine()) {
+            String linea = teclado.nextLine();
+            String[] partes = linea.split(",");
+            if (partes.length == 2) {
+                String nombre = partes[0];
+                String contraseña = partes[1].trim();
+                // Aquí puedes almacenar el nombre y la contraseña en tus arreglos o listas
+            }
+        }
+        sc.close();
+    }
+
+
+
+
